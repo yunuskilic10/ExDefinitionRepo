0a7886234c4f181738f7875f1a11cef8c5e9fa2a
yunus kılıç <yunuskilic2966@gmail.com>
2025-05-07 17:54:17 +0300

    Cucumber yüklendi. Definitons dosyaları oluşturuldu.

---
+++ src/test/java/StepDefinitons/_01_LoginStep.java
@@ -0,0 +1,20 @@
+package StepDefinitons;
+
+
+import io.cucumber.java.en.*;
+
+public class _01_LoginStep {
+    @Given("Navigate to Opencart")
+    public void navigateToOpencart() {
+
+    }
+
+    @When("Enter username and password and click login button")
+    public void enterUsernameAndPasswordAndClickLoginButton() {
+    }
+
+    @Then("User should login successfully")
+    public void userShouldLoginSuccessfully() {
+
+    }
+}
