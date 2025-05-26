9a88ad5763c18c22d3f323a5a4e2e62a3a57ab01
yunus kılıç <yunuskilic2966@gmail.com>
2025-05-26 23:06:00 +0300

    Parale test ve smoke ve regresion test beraber

--- src/test/java/Runners/_07_RunnerExtentReport.java
+++ src/test/java/Runners/_07_RunnerExtentReport.java
@@ -4,10 +4,14 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags ="@RegressionTest",
+        tags ="@RegressionTest and @SmokeTest",
         features = {"src/test/java/Features"},
         glue = {"StepDefinitons"},
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+        plugin = {
+                "pretty",
+                "json:target/cucumber.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        }
 )
 
 public class _07_RunnerExtentReport extends AbstractTestNGCucumberTests {
